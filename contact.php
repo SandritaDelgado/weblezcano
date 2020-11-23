0f1260c5530e61456c409580ce74bceb3b9f1fcb
Sandra Delgado <sandra.delgado1@alu.uclm.es>
2020-11-23 19:27:07 +0100

    confirma modificacion again

--- contact.php
+++ contact.php
@@ -13,10 +13,10 @@ $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'danidelma92@gmail.com';
+$para = 'sandra_bonifa@hotmail.com';
 $asunto = 'Mensaje de mi sitio web';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
 
-header("Location:index.html");
+header("Location:confirma.html");
 ?>
