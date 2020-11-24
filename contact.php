1c09161478ff8cb8ee7f6bf2f5f477f92e5d7f1f
Sandra Delgado <sandra.delgado1@alu.uclm.es>
2020-11-24 12:32:09 +0100

    correo commit

--- contact.php
+++ contact.php
@@ -5,8 +5,51 @@
 //mail('', 'Mensaje de mi sitio web','hollaaa','Holi San');
 //subelo
 
-$exito = mail("sandra_bonifa@hotmail.com","Recuperación de contraseña", "Actualmente podr");
+//$exito = mail("sandra_bonifa@hotmail.com","Recuperación de contraseña", "Actualmente podr");
 //echo "que tal";
+
+// Varios destinatarios
+//vamos a ver primero que se envie el mensaje
+$para = 'sandra_bonifa@hotmail.com';
+
+// título
+$título = 'Recordatorio de cumpleaños para Agosto';
+
+// mensaje
+$mensaje = '
+<html>
+<head>
+  <title>Recordatorio de cumpleaños para Agosto</title>
+</head>
+<body>
+  <p>¡Estos son los cumpleaños para Agosto!</p>
+  <table>
+    <tr>
+      <th>Quien</th><th>Día</th><th>Mes</th><th>Año</th>
+    </tr>
+    <tr>
+      <td>Joe</td><td>3</td><td>Agosto</td><td>1970</td>
+    </tr>
+    <tr>
+      <td>Sally</td><td>17</td><td>Agosto</td><td>1973</td>
+    </tr>
+  </table>
+</body>
+</html>
+';
+
+// Para enviar un correo HTML, debe establecerse la cabecera Content-type
+$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
+$cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Cabeceras adicionales
+$cabeceras .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
+$cabeceras .= 'From: Recordatorio <cumples@example.com>' . "\r\n";
+$cabeceras .= 'Cc: birthdayarchive@example.com' . "\r\n";
+$cabeceras .= 'Bcc: birthdaycheck@example.com' . "\r\n";
+
+// Enviarlo
+$exito = mail($para, $título, $mensaje, $cabeceras);
 
 if ($exito){
     header("Location:confirma.html");
