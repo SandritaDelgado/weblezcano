1d4fe3c54a6ad1ea86afe3cb44abe250c0f1637c
Sandra Delgado <sandra.delgado1@alu.uclm.es>
2020-11-24 12:08:02 +0100

    modificacion contacto

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php
 $nombre = $_POST['nombre'];
 $mail = $_POST['email'];
-$empresa = $_POST['mensaje'];
+$empresa = $_POST['consulta'];
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -10,7 +10,7 @@ $header .= "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
 $mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['consulta'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
 $para = 'sandra_bonifa@hotmail.com';
