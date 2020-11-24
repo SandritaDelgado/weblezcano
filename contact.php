1285044d5ebce4dd1bfdbf09519fa97d5b0f6d2c
Sandra Delgado <sandra.delgado1@alu.uclm.es>
2020-11-24 12:20:09 +0100

    empezando contacto de nuevo

--- contact.php
+++ contact.php
@@ -1,6 +1,7 @@
 <?php
-
-mail('sandra_bonifa@hotmail.com', 'Mensaje de mi sitio web','Holi San');
+//a ver ahora, necesita 4 campos, lo del php es para darle formato, pero con una sola orden deberia
+//mandarte el correo. siii
+mail('sandra_bonifa@hotmail.com', 'Mensaje de mi sitio web','hollaaa','Holi San');
 //subelo
 header("Location:confirma.html");
 ?>
