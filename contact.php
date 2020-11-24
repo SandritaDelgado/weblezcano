0b4635aab702377b53130b0377f062e061b18d03
Sandra Delgado <sandra.delgado1@alu.uclm.es>
2020-11-24 12:28:57 +0100

    commit correito

--- contact.php
+++ contact.php
@@ -1,7 +1,17 @@
 <?php
 //a ver ahora, necesita 4 campos, lo del php es para darle formato, pero con una sola orden deberia
 //mandarte el correo. siii
-mail('sandra_bonifa@hotmail.com', 'Mensaje de mi sitio web','hollaaa','Holi San');
+//vamos a ver si llegar aqui
+//mail('', 'Mensaje de mi sitio web','hollaaa','Holi San');
 //subelo
-header("Location:confirma.html");
+
+$exito = mail("sandra_bonifa@hotmail.com","Recuperación de contraseña", "Actualmente podr");
+//echo "que tal";
+
+if ($exito){
+    header("Location:confirma.html");
+} else {
+    echo "no encuentro una mierda";
+}
+
 ?>
